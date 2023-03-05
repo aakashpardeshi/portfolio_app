d92e33e3a1b799cbbc6938809fb0245804d1bae8
Aakash Pardeshi <aakashspardeshi02@gmail.com>
2023-03-05 14:25:37 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+
+import Header from './Components/Header';
+import Description from './Components/Description';
+
+function App() {
+  return (
+    <div className="App outline">
+      <Header></Header>
+      <Description></Description>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Components/Description.js
@@ -0,0 +1,15 @@
+import './CSS_FILES/Description.css'
+
+function Description(){
+    return(
+        <div className="container">
+            <pre>Hi, myself Aakash Pardeshi I am a 3rd year engineering student pursuing Btech in Information Technology
+                from Sardar Patel Institute of technology
+            </pre>
+
+
+        </div>
+    )
+}
+
+export default Description;
